ff0b505e48e3f63d9cfa00c5c660c0d997a7be6a
xch <3252323572@qq.com>
2023-04-15 21:49:43 +0800

    xch

--- src/test/java/com/xch/interview/InterviewApplicationTests.java
+++ src/test/java/com/xch/interview/InterviewApplicationTests.java
@@ -1,13 +1,31 @@
 package com.xch.interview;
 
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class InterviewApplicationTests {
+    @Resource
+    private MongoTemplate mongoTemplate;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     @Test
     void contextLoads() {
+//        boolean comment1 = mongoTemplate.collectionExists("comment");
+//        System.out.println(comment1);
+
+        Long add = stringRedisTemplate.opsForSet().add("nnnnnn", "serfgsefgsdfgsf");
+        stringRedisTemplate.opsForValue().set("sdfgsd","sdfgsdfgsfgsfdg");
+        System.out.println(add);
+
     }
 
 }
